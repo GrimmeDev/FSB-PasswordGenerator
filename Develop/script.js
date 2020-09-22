8bb598a525a731e1a16559788b24dbc49750e49f
Robert Grimes <rjgrimes@gmail.com>
2020-09-22 14:57:11 -0500

    Initial pseudo code inside of script for planning purposes.

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,30 @@ function writePassword() {
 
 }
 
+// password generation
+function generatePassword(){
+// set maximum length of password, user prompt?
+// repeat prompt is length is less than 8 or larger than 128 characters
+
+
+// bool prompt for lowercase characters
+
+
+// bool prompt for upper case characters
+
+
+// bool prompt for numerical characters
+
+
+// bool prompt for special characters
+
+
+// log out validation of user choices (desk-check purposes only)
+
+
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
